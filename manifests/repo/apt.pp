74a27fc70fd1fef734938a205a1a44189393be09
Vincent <Vincent--@users.noreply.github.com>
2015-08-25 11:42:01 +0100

    Apt v2.0
    Updated apt::source call to support new v2.0.0 module version
    See https://forge.puppetlabs.com/puppetlabs/apt/changelog#2015-04-07---supported-release-200

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -31,9 +31,13 @@ class erlang::repo::apt(
   Class['erlang::repo::apt'] -> Package<| title == $package_name |>
 
   apt::source { 'erlang':
-    include_src => false,
-    key         => $key_signature,
-    key_source  => $remote_repo_key_location,
+    include     => { 
+      'src' => false,
+    },
+    key         => { 
+      'id'     => $key_signature,
+      'source' => $remote_repo_key_location,
+    },
     location    => $remote_repo_location,
     repos       => $repos,
   }
